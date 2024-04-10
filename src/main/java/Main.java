34bfb86e136f2080082bb649850046004137dbde
Janith Rukmal <124907744+janithar122@users.noreply.github.com>
2024-04-10 13:16:20 +0530

    pass 6th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,64 +10,65 @@ public class Main {
 
   final static String CRLF = "\r\n";
 
-  private static void handler(Socket clientSocket) throws IOException{
+  private static void handler(Socket clientSocket) throws IOException {
     InputStream inputStream = clientSocket.getInputStream();
-       InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-       BufferedReader bufferredRead = new BufferedReader(inputStreamReader);
-       String[] request = bufferredRead.readLine().split(" ");
-       String path = request[1];
+    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+    BufferedReader bufferredRead = new BufferedReader(inputStreamReader);
+    String[] request = bufferredRead.readLine().split(" ");
+    String path = request[1];
 
-       OutputStream outputStream = clientSocket.getOutputStream();
-       if (path.equals("/")) {
-        outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-       }else if(path.contains("/echo/")){
-        String msg = path.substring(6);
-        String contentType = "Content-Type: text/plain";
-        String contentLength = "Content-Length: " +msg.length();
+    OutputStream outputStream = clientSocket.getOutputStream();
+    if (path.equals("/")) {
+      outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+    } else if (path.contains("/echo/")) {
+      String msg = path.substring(6);
+      String contentType = "Content-Type: text/plain";
+      String contentLength = "Content-Length: " + msg.length();
 
-        outputStream.write(("HTTP/1.1 200 OK" + CRLF + contentType + CRLF + contentLength + CRLF + CRLF + msg).getBytes());
-       }else if(path.contains("/user-agent")){
-        bufferredRead.readLine();
-        String msg = bufferredRead.readLine().split("User-Agent: ")[1];
-        String contentType = "Content-Type: text/plain";
-        String contentLength = "Content-Length: " +msg.length();
+      outputStream
+          .write(("HTTP/1.1 200 OK" + CRLF + contentType + CRLF + contentLength + CRLF + CRLF + msg).getBytes());
+    } else if (path.contains("/user-agent")) {
+      bufferredRead.readLine();
+      String msg = bufferredRead.readLine().split("User-Agent: ")[1];
+      String contentType = "Content-Type: text/plain";
+      String contentLength = "Content-Length: " + msg.length();
 
-        outputStream.write(("HTTP/1.1 200 OK" + CRLF + contentType + CRLF + contentLength + CRLF + CRLF + msg).getBytes());
-       }else{
-        outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-       }
-       outputStream.flush();
+      outputStream
+          .write(("HTTP/1.1 200 OK" + CRLF + contentType + CRLF + contentLength + CRLF + CRLF + msg).getBytes());
+    } else {
+      outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+    }
+    outputStream.flush();
   }
 
   public static void main(String[] args) {
-    
+
     System.out.println("Logs from your program will appear here!");
-    
-     ServerSocket serverSocket = null;
-     //Socket clientSocket = null;
-     
-    
+
+    ServerSocket serverSocket = null;
+    // Socket clientSocket = null;
+
     try {
       serverSocket = new ServerSocket(4221);
-       serverSocket.setReuseAddress(true);
-       System.out.println("accepted new connection");
-       
-       while (true) {
+      serverSocket.setReuseAddress(true);
+
+      while (true) {
         Socket clientSocket = serverSocket.accept();
-        new Thread(new Runnable(){
+        System.out.println("accepted new connection");
+        new Thread(new Runnable() {
           @Override
-          public void run(){
+          public void run() {
             try {
               handler(clientSocket);
             } catch (Exception e) {
               System.out.println("IOException: " + e.getMessage());
             }
           }
-        });
-       }
+        }).start();
+      }
 
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    }
   }
 }
